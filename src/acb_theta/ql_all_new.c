323e4df00ef6f387f014008ef49da78dbe009bda
Jean Kieffer <jean.kieffer@math.cnrs.fr>
2024-08-29 16:50:35 +0200

    Add duplication in ql_all_new

--- src/acb_theta/ql_all_new.c
+++ src/acb_theta/ql_all_new.c
@@ -13,6 +13,37 @@
 #include "acb_mat.h"
 #include "acb_theta.h"
 
+static void
+acb_theta_ql_dupl(acb_ptr th2, acb_srcptr th0, acb_srcptr th, slong g, slong prec)
+{
+    slong n = 1 << g;
+    acb_ptr v;
+    ulong a, b;
+
+    v = _acb_vec_init(n);
+
+    for (a = 0; a < n; a++)
+    {
+        _acb_vec_set(v, th, n);
+        for (b = 0; b < n; b++)
+        {
+            if (acb_theta_char_dot(a, b, g) % 2 == 1)
+            {
+                acb_neg(&v[b], &v[b]);
+            }
+        }
+        acb_theta_agm_mul(v, th0, v, g, prec);
+        for (b = 0; b < n; b++)
+        {
+            acb_set(&th2[b * n + a], &v[b]);
+        }
+    }
+    _acb_vec_scalar_mul_2exp_si(th2, th2, n * n, g);
+
+    _acb_vec_clear(v, n);
+}
+
+
 int
 acb_theta_ql_all_new(acb_ptr th, acb_srcptr zs, slong nb, const acb_mat_t tau,
     int sqr, slong prec)
@@ -62,7 +93,11 @@ acb_theta_ql_all_new(acb_ptr th, acb_srcptr zs, slong nb, const acb_mat_t tau,
         res = acb_theta_ql_exact(new_th, new_z, nb + add_zero, new_tau, 0, 0, prec);
         if (res)
         {
-
+            for (j = 0; j < nb; j++)
+            {
+                acb_theta_ql_dupl(th + j * n * n, new_th,
+                    new_th + (j + add_zero) * n, g, prec);
+            }
         }
         acb_mat_scalar_mul_2exp_si(new_tau, new_tau, -1);
         _acb_vec_scalar_mul_2exp_si(new_z, new_z, (nb + add_zero) * g, 1);
