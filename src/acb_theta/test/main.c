2d045bb05abaf39e7a98fb1e9a2b9322cea6f5c4
Jean Kieffer <jean.kieffer@math.cnrs.fr>
2024-09-04 12:23:14 +0200

    Remove t-sum_0b; tested through sum_all_tilde

--- src/acb_theta/test/main.c
+++ src/acb_theta/test/main.c
@@ -82,7 +82,6 @@
 #include "t-sp2gz_is_correct.c"
 #include "t-sp2gz_set_blocks.c"
 #include "t-sum_00.c"
-#include "t-sum_0b.c"
 #include "t-sum_a0_tilde.c"
 #include "t-sum_all_tilde.c"
 #include "t-sum_jet_00.c"
@@ -167,7 +166,6 @@ test_struct tests[] =
     TEST_FUNCTION(acb_theta_sp2gz_is_correct),
     TEST_FUNCTION(acb_theta_sp2gz_set_blocks), */
     TEST_FUNCTION(acb_theta_sum_00),
-    TEST_FUNCTION(acb_theta_sum_0b),
     TEST_FUNCTION(acb_theta_sum_a0_tilde),
     TEST_FUNCTION(acb_theta_sum_all_tilde),
     TEST_FUNCTION(acb_theta_sum_jet_00),
